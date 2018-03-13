d0a272ee5a356410479e683db6076a9f853af29f
Aaron Gooden <aagooden@me.com>
2018-03-13 14:09:03 -0400

    Initial Commit

---
+++ code_word.rb
@@ -0,0 +1,4 @@
+def code_word
+	meaning = ''
+	return meaning
+end
---
+++ test_code_word.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "code_word.rb"
+
+class Code_word_test < Minitest::Test
+
+	def test_boolean
+		assert_equal(true, true)
+	end
+	
+end
