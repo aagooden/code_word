ef9288a6f5df1b7cad1605efb1f189eb20aba536
Aaron Gooden <aagooden@me.com>
2018-03-13 14:58:53 -0400

    Added functionality and tests passing

--- code_word.rb
+++ code_word.rb
@@ -1,4 +1,12 @@
-def code_word
-	meaning = ''
-	return meaning
+def code_word(error)
+	error_description = ''
+	error_hash = {404 => "Page Not Found", 1942 => "Grandpa was found", 402 => "Page almost found"}
+	
+	error_hash.each_key { |key|
+		if key == error
+			error_description = error_hash[error]
+		end
+	}
+
+	return error_description
 end
--- test_code_word.rb
+++ test_code_word.rb
@@ -6,5 +6,17 @@ class Code_word_test < Minitest::Test
 	def test_boolean
 		assert_equal(true, true)
 	end
-	
+
+	def test_return_string
+		assert_equal(String, code_word(404).class)
+	end	
+
+	def test_return_code_Page_Not_Found
+		assert_equal("Page Not Found", code_word(404))
+	end	
+
+	def test_return_code_Grandpa_was_found
+		assert_equal("Grandpa was found", code_word(1942))
+	end	
+
 end
